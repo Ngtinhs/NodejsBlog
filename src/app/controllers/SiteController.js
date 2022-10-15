602732efead4f571e3ae1d55f74acc16746f5713
ngtinh <nguyenvanductinh77@gmail.com>
2022-10-15 14:16:08 +0700

    [1]Setup MVC

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,19 @@
+const Course = require('../models/Course');
+
+class SiteController {
+    // [GET] /
+    index(req, res, next) {
+        Course.find({})
+        .then (courses => res.render('home'))
+        .catch (next)
+
+        // res.render('home');
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('search');
+    }
+}
+
+module.exports = new SiteController();
