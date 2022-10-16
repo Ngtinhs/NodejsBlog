72662d04858132dd6861c87e037cafffc5969fbc
ngtinh <nguyenvanductinh77@gmail.com>
2022-10-16 10:52:54 +0700

    [4]Tao moi khoa hoc]

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,7 +1,7 @@
 const Course = require('../models/Course');
 const { mongooseToObject } = require('../../until/mongoose');
 class CourseController {
-    // [GET] /search
+    // [GET] /course/:slug
     show(req, res, next) {
         Course.findOne({
             slug: req.params.slug,
@@ -12,6 +12,21 @@ class CourseController {
                 }),
             )
             .catch(next);
+    }
+
+    // [GET] /course/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+    // [POST] /course/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/'))
+            .catch((error) => {});
     }
 }
 
